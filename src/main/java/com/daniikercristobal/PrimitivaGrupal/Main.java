386b67ec5ca35f3b8c40192414f8f0dd631cd786
Dani Minguet <daminto2001@gmail.com>
2022-03-09 23:38:14 +0100

    Creacion menú principal

--- src/main/java/com/daniikercristobal/PrimitivaGrupal/Main.java
+++ src/main/java/com/daniikercristobal/PrimitivaGrupal/Main.java
@@ -1,7 +1,9 @@
 package com.daniikercristobal.PrimitivaGrupal;
 
+import com.daniikercristobal.PrimitivaGrupal.Primitiva.Principal;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Principal principal = new Principal();
     }
 }
--- src/main/java/com/daniikercristobal/PrimitivaGrupal/Primitiva/Principal.java
+++ src/main/java/com/daniikercristobal/PrimitivaGrupal/Primitiva/Principal.java
@@ -1,5 +1,32 @@
 package com.daniikercristobal.PrimitivaGrupal.Primitiva;
 
-public class Principal {
+import com.daniikercristobal.PrimitivaGrupal.Lib.Lib;
 
+public class Principal {
+    public Principal() {
+
+    }
+
+    private boolean menuPrincipal() {
+        boolean resultado = false, validado = false;
+        char opcion;
+
+        do {
+            System.out.println("***************");
+            System.out.println("** PRIMITIVA **");
+            System.out.println("***************");
+            opcion = Lib.leerChar("Quieres introducir tú mismo los números con los que deseas jugar? (s/n): ");
+            if (opcion == 's') {
+                validado = true;
+                resultado = true;
+            } else if (opcion == 'n') {
+                validado = true;
+                resultado = false;
+            } else {
+                System.out.println("Debes introducir 's' o 'n'");
+            }
+        } while (!validado);
+
+        return resultado;
+    }
 }
